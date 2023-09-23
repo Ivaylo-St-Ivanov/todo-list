5d58a1ccc6f3dd027f1cce4a8deeb11535458329
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2023-09-23 19:10:21 +0300

    Add initial HTML & CSS.

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,13 @@ import './App.css';
 
 function App() {
     return (
-        <div>
-            
+        <div className="container" >
+            <form>
+                <input className="input-field" type="text" name="text" placeholder=" ....." />
+                <input className="btn" type="submit" name="btn" value="Add new task" />
+            </form>
+
+            <hr />
         </div>
     );
 }

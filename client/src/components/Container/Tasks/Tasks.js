f7f94f4ea8fc281474402f624d5570699bd4afca
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2023-10-17 13:16:20 +0300

    Added "cross out task" functionality.

--- client/src/components/Container/Tasks/Tasks.js
+++ client/src/components/Container/Tasks/Tasks.js
@@ -1,16 +1,30 @@
+import { useState } from 'react';
+
 import { Checkbox, Delete, Edit, TaskName, TaskRow, TasksBox } from './styled';
 
 const Tasks = ({
     tasks
 }) => {
+    const [isComplete, setIsComplete] = useState(false);
+
+    const onClickCheckBox = () => {
+        setIsComplete(state => !state);
+    };
+
     return (
         <TasksBox>
             {tasks.map(t => (
                 <TaskRow key={t.objectId}>
-                    <TaskName>{t.task_name}</TaskName>
+                    {!isComplete && (
+                        <TaskName>{t.task_name}</TaskName>
+                    )}
+
+                    {isComplete && (
+                        <TaskName><s>{t.task_name}</s></TaskName>
+                    )}
 
                     <div>
-                        <Checkbox type="checkbox" name="completed" id="completed" />
+                        <Checkbox onClick={onClickCheckBox} type="checkbox" name="completed" id="completed" />
                         <Edit className="fas fa-edit" />
                         <Delete className="fas fa-trash-alt" style={{ color: '#ff0000' }} />
                     </div>
